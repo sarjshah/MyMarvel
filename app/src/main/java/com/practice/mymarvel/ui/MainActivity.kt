b726456de5b979e7f5863d561f5951d039049621
sarjshah <59883747+sarjshah@users.noreply.github.com>
2021-05-04 22:05:55 +0100

    Ran Ktlint

--- app/src/main/java/com/practice/mymarvel/ui/MainActivity.kt
+++ app/src/main/java/com/practice/mymarvel/ui/MainActivity.kt
@@ -1,21 +1,18 @@
 package com.practice.mymarvel.ui
 
-import android.content.Context
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.LayoutInflater
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.practice.mymarvel.R
 import com.practice.mymarvel.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding:ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil
+            .setContentView(this, R.layout.activity_main)
 
         with(binding) {
             tvHello.text = "Hello from Marvel3"
