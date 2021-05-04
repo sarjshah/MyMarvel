fc02ee8becc9796fabdc5a0382e5fa614571dff5
sarjshah <59883747+sarjshah@users.noreply.github.com>
2021-05-04 21:45:48 +0100

    Added Databinding functionality.

--- app/src/main/java/com/practice/mymarvel/ui/MainActivity.kt
+++ app/src/main/java/com/practice/mymarvel/ui/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.practice.mymarvel.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.practice.mymarvel.R
+import com.practice.mymarvel.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding:ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        tvHello.text = "Hello from Marvel"
+        with(binding) {
+            tvHello.text = "Hello from Marvel3"
+        }
     }
 }
